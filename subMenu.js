4c44a0ea874c8d3adaec6ab83375e1094cd9d278
baontraan <baotran221003@gmail.com>
2024-06-21 01:49:36 +0700

    update 21/6/2024

--- subMenu.js
+++ subMenu.js
@@ -4,5 +4,3 @@ sideBar.addEventListener("click", function () {
   sideBar.classList.toggle("active");
   subMenu.classList.toggle("active");
 });
-const body = document.querySelector("body");
-console.log(body);
